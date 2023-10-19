3a6a898f5791a8deb2b836a9eeb0f51266fe534b
Felipe Padilla <padilla.f@northeastern.edu>
2023-10-19 13:15:00 -0400

    working on a3 lab

---
+++ src/Labs/a3/JavaScript.js
@@ -0,0 +1,16 @@
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+import BooleanVariables from "./BooleanVariables";
+
+function JavaScript() {
+    console.log('Hello World!');
+    return (
+        <div>
+            <h1>JavaScript</h1>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+        </div>
+    );
+}
+export default JavaScript
