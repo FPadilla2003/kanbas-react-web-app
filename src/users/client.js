ded7fba95a1e2ee58394c8e90ded23ca7d7357bd
Felipe Padilla <padilla.f@northeastern.edu>
2023-12-05 12:52:44 -0500

    a6 2

--- src/users/client.js
+++ src/users/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
     withCredentials: true,
 });
-export const BASE_API = "https://kanbas-node-server-app-aba3.onrender.com";
+export const BASE_API = process.env.REACT_APP_BASE_API || "http://localhost:4000";
 export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
     const response = await request.post(`${USERS_API}/signin`, credentials);
